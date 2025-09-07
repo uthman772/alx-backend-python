0586552ebd298ded3e26b1b8629a82a1900bf2f4
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-07 21:03:26 +0000

    Add initial implementation of stream_users function

---
+++ python-generators-0x00/python-generators-0x00/0-stream_users.py
@@ -0,0 +1,7 @@
+def stream_users():
+    # Connect to database
+    # Query user_data
+    # Yield each row as a dictionary
+{'user_id': '00234e50...', 'name': 'Dan Altenwerth Jr.', 'email': 'Molly59@gmail.com', 'age': 67}
+{'user_id': '006bfede...', 'name': 'Glenda Wisozk', 'email': 'Miriam21@gmail.com', 'age': 119}
+...
