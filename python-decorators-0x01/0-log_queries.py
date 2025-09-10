b32ef565784b35f31e686ef77496f0be76ff25c7
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-10 19:30:15 +0000

    Refactor log_queries decorator to improve SQL query logging functionality

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,11 +1,12 @@
-def log_queries()
-    import sqlite3
-    import functools
+import functools
 
-#### decorator to lof SQL queries
-
- """ YOUR CODE GOES HERE"""
-
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query') or args[0]
+        print(f"Executing SQL query: {query}")
+        return func(*args, **kwargs)
+    return wrapper
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
@@ -14,3 +15,6 @@ def fetch_all_users(query):
     results = cursor.fetchall()
     conn.close()
     return results
+
+users = fetch_all_users(query="SELECT * FROM users")
+
