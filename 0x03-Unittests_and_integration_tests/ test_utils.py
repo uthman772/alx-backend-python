6c1e05818f3b42e6cc94b578f547a48bf0b12d75
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-17 22:22:54 +0000

    Add additional test cases for nested map handling

--- 0x03-Unittests_and_integration_tests/ test_utils.py
+++ 0x03-Unittests_and_integration_tests/ test_utils.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
 nested_map={"a": 1}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a",)
 nested_map={"a": {"b": 2}}, path=("a", "b")
+nested_map={"a": {"b": {"c": 3}}}, path=("a", "b", "c")
+nested_map={"a": {"b": {"c": {"d": 4}}}}, path=("a", "b", "c", "d")
+nested_map={"x": {"y": {"z": 0}}}, path=("x", "y", "z")
+nested_map={"key1": {"key2": {"key3": "value"}}}, path=("key1", "key2", "
