3a49c8f3d9da53871c14be3137faf6f8c0f0c6c2
usaman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-09-17 16:22:37 -0700

    Update  test_utils.py

--- 0x03-Unittests_and_integration_tests/ test_utils.py
+++ 0x03-Unittests_and_integration_tests/ test_utils.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python3
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 
-nested_map={"a": 1}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a",)
-nested_map={"a": {"b": 2}}, path=("a", "b")
-nested_map={"a": {"b": {"c": 3}}}, path=("a", "b", "c")
-nested_map={"a": {"b": {"c": {"d": 4}}}}, path=("a", "b", "c", "d")
-nested_map={"x": {"y": {"z": 0}}}, path=("x", "y", "z")
-nested_map={"key1": {"key2": {"key3": "value"}}}, path=("key1", "key2", "
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test class for access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the expected result"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
