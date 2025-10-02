193df63d443e6b4acbff39cb1a59fec341fcfc17
usman yau Muhammad <79041451+uthman772@users.noreply.github.com>
2025-10-02 16:26:36 -0700

    Update urls.py

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path
-from . import views
-
-app_name = 'messaging'
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('message/<int:message_id>/', views.message_detail, name='message_detail'),
-    path('api/message/<int:message_id>/history/', views.message_history_api, name='message_history_api'),
-    
-    # User account deletion URLs
-    path('account/delete/confirmation/', views.delete_account_confirmation, name='delete_account_confirmation'),
-    path('account/delete/', views.delete_account, name='delete_account'),
-    path('api/user/data/summary/', views.user_data_summary, name='user_data_summary'),
+    path('admin/', admin.site.urls),
+    path('messaging/', include('messaging.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
